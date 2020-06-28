75f631483ead05415dc8649af0a1a5cd6ffcfae9
zachary mines <zmines@wesleyan.edu>
2020-06-28 03:59:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -99,7 +99,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, stuff|
     stuff.each do |kees,val|
-      if 
+      if val.include?()
 end
 
 
